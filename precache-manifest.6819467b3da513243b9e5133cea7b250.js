59fdb64f0149a3c0a9530201aa970a37613ebf69
Miroljub <miroljub1995@gmail.com>
2020-03-31 14:42:58 +0200

    Updates

---
+++ precache-manifest.6819467b3da513243b9e5133cea7b250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8e7d7966adb8132fcd595583ce9b33",
+    "url": "/test-gh-pages/index.html"
+  },
+  {
+    "revision": "30b06d45e0d9bab92cbc",
+    "url": "/test-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "691e729dd36d7e12d52b",
+    "url": "/test-gh-pages/static/js/2.57b5f145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-gh-pages/static/js/2.57b5f145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b06d45e0d9bab92cbc",
+    "url": "/test-gh-pages/static/js/main.58b6618b.chunk.js"
+  },
+  {
+    "revision": "eaa652c4e876c4641eab",
+    "url": "/test-gh-pages/static/js/runtime-main.6dab942e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dee2faeb6864a2ebf220c5b19392983b.js"
+  "/test-gh-pages/precache-manifest.6819467b3da513243b9e5133cea7b250.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-gh-pages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
