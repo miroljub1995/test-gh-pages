343b3915b032eb61b1310e7e258fa692f0f31ced
Miroljub <miroljub1995@gmail.com>
2020-03-31 14:17:31 +0200

    Updates

---
+++ precache-manifest.dee2faeb6864a2ebf220c5b19392983b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e60c6cd53f9b266d03a3c6e620d18ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d54e294101fa042018ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "691e729dd36d7e12d52b",
+    "url": "/static/js/2.57b5f145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57b5f145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54e294101fa042018ad",
+    "url": "/static/js/main.fe7ab934.chunk.js"
+  },
+  {
+    "revision": "ee4469d12b8179f34e61",
+    "url": "/static/js/runtime-main.dc45e108.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
